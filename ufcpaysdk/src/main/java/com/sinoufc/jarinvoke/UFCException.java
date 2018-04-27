94a1582590afa6001f6db38e695a689b70e7262f
jiangzhengyan <jzy_yany@163.com>
2018-04-27 10:22:10 +0800

    提交的内容

--- ufcpaysdk/src/main/java/com/sinoufc/jarinvoke/UFCException.java
+++
@@ -1,9 +0,0 @@
-package com.sinoufc.jarinvoke;
-
-public class UFCException extends Exception {
-
-	public UFCException(String detailMessage) {
-		super(detailMessage);
-	}
-
-}
--- ufcpaysdk/src/main/java/com/sinoufc/jarinvoke/async/UFCResult.java
+++
@@ -1,7 +0,0 @@
-package com.sinoufc.jarinvoke.async;
-/**
- * 支付或者查询结果接口
- */
-public interface UFCResult {
-
-}
