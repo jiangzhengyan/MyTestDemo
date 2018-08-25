e727fbf3b96c28d52c673ee34517f88ac574f409
jiangzhengyan <451704940@qq.com>
2018-08-25 15:28:08 +0800

    添加扫描二维码

--- app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_2_3.java
+++ app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_2_3.java
@@ -1,9 +1,6 @@
 package com.jingcaiwang.mytestdemo.activities;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -11,8 +8,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -21,13 +16,15 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.jingcaiwang.mytestdemo.R;
-import com.jingcaiwang.mytestdemo.application.MyApplication;
+import com.jingcaiwang.mytestdemo.views.loopview.LoopView;
+import com.jingcaiwang.mytestdemo.views.loopview.OnItemSelectedListener;
 import com.zbar.lib.CaptureActivity;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -37,12 +34,110 @@ public class Activity_2_3 extends AppCompatActivity {
 
     @Bind(R.id.iv_pic)
     ImageView mIvPic;
+    @Bind(R.id.loopView_date)
+    LoopView mLoopViewDate;
+    @Bind(R.id.loopView_start_time)
+    LoopView mLoopViewStartTime;
+    @Bind(R.id.loopView_end_time)
+    LoopView mLoopViewEndTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2_3);
         ButterKnife.bind(this);
+        createData();
+    }
+
+    private String dateSelect;
+    private String startTimeSelect;
+    private String endTimeSelect;
+
+    private void createData() {
+        ArrayList<String> dateList = new ArrayList<>();
+        ArrayList<String> startTimeList = new ArrayList<>();
+        ArrayList<String> endTimeList = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        //calendar.clear();//避免继承当前系统的时间
+        for (int i = 0; i < 14; i++) {
+            calendar.clear();//避免继承当前系统的时间
+            calendar.setTime(new Date());
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + i);
+            int year = calendar.get(Calendar.YEAR);
+            int monthOfYear = calendar.get(Calendar.MONTH) + 1;
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+
+            dateList.add(monthOfYear + "月" + dayOfMonth + "日");
+            // Log.e(TAG, "createData: " + monthOfYear + "月" + dayOfMonth + "日");
+        }
+
+        for (int i = 0; i < 24; i++) {
+            startTimeList.add(i + ":" + "00");
+            startTimeList.add(i + ":" + "30");
+            endTimeList.add(i + ":" + "00");
+            endTimeList.add(i + ":" + "30");
+        }
+
+        setLoopView(mLoopViewDate, dateList);
+        setLoopView(mLoopViewStartTime, startTimeList);
+        setLoopView(mLoopViewEndTime, endTimeList);
+        //初始化位置
+        //initLoopView(mLoopViewDate ,dateList);
+        initLoopView(mLoopViewStartTime, startTimeList);
+        initLoopView(mLoopViewEndTime, endTimeList);
+        //选择日期
+        dateSelect = dateList.get(mLoopViewDate.getSelectedItem());
+        mLoopViewDate.setListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                dateSelect = dateList.get(index);
+            }
+        });
+        //选择开始时间
+        startTimeSelect = startTimeList.get(mLoopViewStartTime.getSelectedItem());
+        mLoopViewStartTime.setListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                startTimeSelect = startTimeList.get(index);
+            }
+        });
+        //选择结束时间
+        endTimeSelect = endTimeList.get(mLoopViewEndTime.getSelectedItem());
+        mLoopViewEndTime.setListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int index) {
+                endTimeSelect = endTimeList.get(index);
+            }
+        });
+
+    }
+
+    /**
+     * //初始化位置
+     *
+     * @param loopView
+     * @param list
+     */
+    private void initLoopView(LoopView loopView, ArrayList<String> list) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        if (minute > 30) {
+            loopView.setCurrentPosition(hour * 2 + 2);
+        } else {
+            loopView.setCurrentPosition(hour * 2 + 1);
+        }
+
+    }
+
+    private void setLoopView(LoopView loopView, ArrayList<String> list) {
+        loopView.setItems(list);
+        loopView.setNotLoop();
+        loopView.setTextSize(17);
     }
 
     // 拍照回传码
@@ -64,8 +159,8 @@ public class Activity_2_3 extends AppCompatActivity {
 
 //        mTempPhotoPath = Environment.getExternalStorageDirectory() + File.separator + "aaaaaa" + File.separator + System.currentTimeMillis() + File.separator + "photo.jpeg";
         //mTempPhotoPath = Environment.getExternalStorageDirectory() + File.separator + "aaaaaa" + File.separator + System.currentTimeMillis() +   "photo.jpeg";
-        mTempPhotoPath =  getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + System.currentTimeMillis() +   "photo.jpeg";
-       // mTempPhotoPath = getExternalCacheDir().getAbsolutePath() + File.separator + "aaaaaa" + File.separator + System.currentTimeMillis() +   "photo.jpg";
+        mTempPhotoPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + System.currentTimeMillis() + "photo.jpeg";
+        // mTempPhotoPath = getExternalCacheDir().getAbsolutePath() + File.separator + "aaaaaa" + File.separator + System.currentTimeMillis() +   "photo.jpg";
         // 获取图片所在位置的Uri路径    *****这里为什么这么做参考问题2*****
         /*imageUri = Uri.fromFile(new File(mTempPhotoPath));*/
         //imageUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".my.provider", new File(mTempPhotoPath));
@@ -87,8 +182,8 @@ public class Activity_2_3 extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             imageUri = FileProvider.getUriForFile(this, "com.jingcaiwang.mytestdemo.fileprovider", file);
-           intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         } else {
             intent.putExtra(MediaStore.EXTRA_OUTPUT,
                     Uri.fromFile(file));
@@ -102,26 +197,28 @@ public class Activity_2_3 extends AppCompatActivity {
         takePhoto();
 
     }
+
     /**
      * 扫码
      */
     @OnClick(R.id.tv_open_xcode)
-      void scanClick(View v) {
+    void scanClick(View v) {
         Intent intent = new Intent(this,
                 CaptureActivity.class);
         startActivity(intent);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.e(TAG, "onActivityResult: " + mTempPhotoPath);
-       // Bitmap bitmap = BitmapFactory.decodeFile("file://" + mTempPhotoPath);
-        Bitmap bitmap = BitmapFactory.decodeFile(  mTempPhotoPath);
+        // Bitmap bitmap = BitmapFactory.decodeFile("file://" + mTempPhotoPath);
+        Bitmap bitmap = BitmapFactory.decodeFile(mTempPhotoPath);
         Toast.makeText(this, "" + mTempPhotoPath + "-----" + bitmap + "  " + new File(mTempPhotoPath).exists(), Toast.LENGTH_LONG).show();
 
 
-       Glide.with(this).load(mTempPhotoPath).into(mIvPic);
-       // Glide.with(this).load(imageUri).into(mIvPic);
+        Glide.with(this).load(mTempPhotoPath).into(mIvPic);
+        // Glide.with(this).load(imageUri).into(mIvPic);
 
     }
 
