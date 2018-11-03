89afbb437fc3bb82bace5f793693cbe48773810b
jiangzhengyan <451704940@qq.com>
2018-11-03 15:19:26 +0800

    滑板

--- app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_4_3.java
+++ app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_4_3.java
@@ -2,13 +2,22 @@ package com.jingcaiwang.mytestdemo.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.jingcaiwang.mytestdemo.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Activity_4_3 extends AppCompatActivity {
 
+
+    @Bind(R.id.lv)
+    ListView mLv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,34 @@ public class Activity_4_3 extends AppCompatActivity {
         setContentView(R.layout.activity_4_3);
         ButterKnife.bind(this);
 
+
+        mLv.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 119;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                TextView textView = new TextView(Activity_4_3.this);
+
+
+                textView.setText("是是是是是是是是是是是是是试试是是是是是是是是是是是是是试试事实上事实上所所所所是是是是是是是是是是是是是试试是是是是是是是是是是是是是试试事实上事实上所所所所");
+                return textView;
+            }
+        });
     }
-
-
 
 
 }
