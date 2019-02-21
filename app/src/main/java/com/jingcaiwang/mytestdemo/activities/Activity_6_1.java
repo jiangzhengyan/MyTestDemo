b741781fd80e36db4d3bdabd274d20fdb85340ac
jiangzhengyan <451704940@qq.com>
2019-02-21 20:59:16 +0800

    滑板

--- app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_6_1.java
+++ app/src/main/java/com/jingcaiwang/mytestdemo/activities/Activity_6_1.java
@@ -1,11 +1,16 @@
 package com.jingcaiwang.mytestdemo.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.widget.Button;
 
 import com.jingcaiwang.mytestdemo.R;
+import com.jingcaiwang.mytestdemo.utils.UserUtil;
+import com.jingcaiwang.mytestdemo.views.passwordinput.OnPasswordInputFinish;
+import com.jingcaiwang.mytestdemo.views.passwordinput.PopEnterPassword;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,13 +35,29 @@ public class Activity_6_1 extends AppCompatActivity {
     @OnClick(R.id.btn)
     public void onViewClicked() {
 
-        Intent intent = new Intent( );
+showPayKeyBoard();
+    }
 
-        intent.setAction(Intent.ACTION_WEB_SEARCH);
-      //  intent.addCategory(Intent.CATEGORY_APP_MAPS);
 
-        startActivity(intent);
+    public void showPayKeyBoard( ) {
 
+        PopEnterPassword popEnterPassword = new PopEnterPassword(Activity_6_1.this);
+
+        // 显示窗口
+      //  popEnterPassword.showAtLocation(this.findViewById(R.id.layoutContent),
+        popEnterPassword.showAtLocation(mBtn,
+
+
+
+                Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0); // 设置layout在PopupWindow中显示的位置
+
+        popEnterPassword.setOnPasswordInputFinish(new OnPasswordInputFinish() {
+            @Override
+            public void inputFinish(String password) {
+
+
+            }
+        });
     }
 }
 
